37c0fa18bd063cb100d3e9c8803b503d9bf57694
Mati OS <matias.ovejasmith@gmail.com>
2025-05-15 20:09:32 -0300

    Add transfers page

--- apps/web/app/(home)/transfers/page.tsx
+++ apps/web/app/(home)/transfers/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Link, Paper, Typography } from "@mui/material";
 
 export default function Home() {
   return (
@@ -13,10 +13,18 @@ export default function Home() {
       <Paper elevation={3} sx={{ p: 4, borderRadius: 2, width: "70%" }}>
 
         <Box mb={4}>
-          <Typography variant="h6" gutterBottom>
-            Send tx initiated. See: https://testnet.layerzeroscan.com/tx/0x27b4a2ff822d68e4284c3a8b15ecfd4f38f0a2930b88c658ca2537b41173be34
+          <Typography>
+            Send tx initiated. See: 
           </Typography>
-
+          <Typography>
+            <Link 
+                href="https://testnet.layerzeroscan.com/tx/0x27b4a2ff822d68e4284c3a8b15ecfd4f38f0a2930b88c658ca2537b41173be34"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              https://testnet.layerzeroscan.com/tx/0x27b4a2ff822d68e4284c3a8b15ecfd4f38f0a2930b88c658ca2537b41173be34
+            </Link>
+          </Typography>
         </Box>
       </Paper>
     </Box>
