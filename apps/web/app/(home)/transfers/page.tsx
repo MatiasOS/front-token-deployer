4d220cac25d07862e956bea59f9b9dfb4a3ae28e
Mati OS <matias.ovejasmith@gmail.com>
2025-05-15 19:46:15 -0300

    Add transfers

---
+++ apps/web/app/(home)/transfers/page.tsx
@@ -0,0 +1,24 @@
+import { Box, Paper, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 4,
+      }}
+    >
+      <Paper elevation={3} sx={{ p: 4, borderRadius: 2, width: "70%" }}>
+
+        <Box mb={4}>
+          <Typography variant="h6" gutterBottom>
+            Send tx initiated. See: https://testnet.layerzeroscan.com/tx/0x27b4a2ff822d68e4284c3a8b15ecfd4f38f0a2930b88c658ca2537b41173be34
+          </Typography>
+
+        </Box>
+      </Paper>
+    </Box>
+  );
+}
